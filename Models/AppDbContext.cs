78f3e4e2c3bfeae54522940ac25bb69eebdb4fc2
Moisessantos45 <santosxphdz34@gmail.com>
2026-03-08 03:40:16 -0600

    feat: Add Juego DbSet

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -6,6 +6,7 @@ public class AppDbContext : DbContext
     public DbSet<Usuario> Usuarios { get; set; } = null!;
     public DbSet<Pregunta> Preguntas { get; set; } = null!;
     public DbSet<Categoria> Categorias { get; set; } = null!;
+    public DbSet<Juego> Juegos { get; set; } = null!;
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 }
