8d79c17ec3c2e796301b8c0a37ebe0dbc66392e8
Moisessantos45 <santosxphdz34@gmail.com>
2026-03-08 02:28:38 -0600

    feat: Categoria model with description and image properties.

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -4,4 +4,6 @@ public class Categoria
 {
     public int Id { get; set; }
     public string Nombre { get; set; } = string.Empty;
+    public string Descripcion { get; set; } = string.Empty;
+    public string Imagen {get; set;} = string.Empty;
 }
